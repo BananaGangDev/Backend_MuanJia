25ef041478182450a6d957b64f4a1991a78dc438
phattradaa <phattrada.mik@dome.tu.ac.th>
2024-04-29 14:45:17 +0700

    improve:order_create

--- myproject/payment/urls.py
+++ myproject/payment/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create',views.create_payment,name='Create Payment'),
+    # path('create',views.create_payment,name='Create Payment'),
     path('get_payment_by_payment_id/<str:id>',views.get_payment_by_id,name='Get Payment By Payment_id'),
     path('update_status/<str:id>/<path:path_slip>',views.update_status,name="Update Payment"),
     path('get_payment_by_order_id/<str:id>',views.get_payment_by_order_id,name='Get Payment By Order_id')
 ]
 
+{"id":"111",
+ "path_slip":"/image/slip.jpg"
+ }
